4a8784c2d38e9bc43044149b1db44a391a256009
Daniel Godoy <danielgodoy66@gmail.com>
2026-06-10 19:29:51 +0200

    update index.html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,27 @@
 import RecipeForm from "./components/RecipeForm";
 
 function App() {
-    return <RecipeForm />;
+    return (
+        <div style={appStyle}>
+                <RecipeForm />
+            <footer style={footerStyle}>
+                <p> Made with ❤️ by{" "} <a href="http://github.com/DanielGodoyGalindo" target="_blank" rel="noopener noreferrer" style={{ color: "#646cff", fontWeight: "bold" }}>Daniel Godoy</a></p>
+            </footer>
+        </div>
+    );
+}
+
+const footerStyle: React.CSSProperties = {
+    margin: "10px",
+    textAlign: "center",
+    fontSize: "14px",
+    color: "#888",
+};
+
+const appStyle: React.CSSProperties = {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
 }
 
 export default App;
